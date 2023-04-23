8469c2b5032903afdabb7787edf412219f5f0c37
arsybai <me@arsybai.com>
2023-04-24 05:37:42 +0700

    Update the documentation and V0.0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="sayuDB",
-    version="0.0.8",
+    version="0.0.9",
     author="Arsybai",
     description="Database management system based on python and JSON.",
     packages=["sayuDB"],
@@ -18,195 +18,298 @@ setuptools.setup(
     'flask'
     ],
     long_description_content_type="text/markdown",
-    long_description="""# Documentation
-
-##### Table of content
-###### Preface
-1. [What is sayuDB](#1_What_is_sayuDB_18)
-
-###### Tutorial
-1. [Getting Started](#1-getting-started)
-- 1.1 [Installation](#11-installation)
-- 1.2 [Creating database](#12-creating-database)
-- 1.3 [Creating a New Table](#13-creating-a-new-table)
-- 1.4 [Populating a table with rows](#14-pupulating-a-table-with-rows)
-- 1.5 [Querying a Table](#15-querying-a-table)
-- 1.6 [Updates](#updates)
-- 1.7 [Deletion](#17-deletion)
-
+    long_description="""![banner](https://images.arsybai.app/images/pOMsOCbxLR.png)
 ---
 # Preface
-### 1. What is sayuDB
+### What is sayuDB
 sayuDB is an database management system based on python and JSON. Developed at Clee Ltd. This project actually for personal purpose only but for some reason I publish it.
 It supports a large part of the SQL standard feature
 
 # Tutorial
-### 1. Getting Started
-#### 1.1 Installation
-Before you can use sayuDB you need to install it, of course.
-Just clone this repository and place it to your project folder.
+##### Table of contents
+
+- [Installation](#installation)
+- [Create or remove user](#Create_or_remove_user)
+- [Grant privileges to user](#Grant_privileges_to_user)
+- [Create and drop database](#Create_and_drop_database)
+- [Import and export database](#Import_and_export_database)
+- [Activate or deactivate server](#Activate_or_deactivate_server)
+- [Using the sayuDB](#Using_the_sayuDB)
+- [Create table](#Create_table)
+- [Drop table](#Drop_table)
+- [Show table](#Show_table)
+- [Insert](#Insert)
+- [Select](#Select)
+- [Where](#Where)
+- [Update](#Update)
+- [Delete](#Delete)
+- [Alter table](#Alter_table)
+- [Clear table](#Clear_table)
+---
+
+### Installation
 ```shell
 > pip3 install sayuDB
 ```
-for help menu:
+and if you want to show help menu just 
+```
+> python3 -m sayuDB --h
+```
+
+### Create or remove user
+To create a user you can command by
 ```shell
-> python3 sayuDB --h
+> python3 -m sayuDB create user <username> <password>
+```
+and if you want to remove user you can command by
+```shell
+> python3 -m sayuDB remove user <username>
+```
+Note that creating or removing user only can be user when you have root/local access.
+the user is use for remote database. if you want to user this database locally, you didn't need this.
+
+And for show user, you can command by
+```shell
+> python3 -m sayuDB show users
 ```
 
-#### 1.2 Creating database
-The first test to see whether you can access the database server is to try to create a database.
-
-To create a new database, in this example named `myDB`, you use the following funtion:
-```python
-import sayuDB
-
-# Creating database
-sayuDB.create_database('myDB')
+### Grant privileges to user
+Ofcourse you need to grant user to some database first before use it remotely.
+Grant it by command
+```shell
+> python3 -m sayuDB grant user <username> <database_name>
 ```
-You can also create databases with other names. sayuDB allows you to create any number of databases at a given site. Database names must have an alphabetic first character, can not contain space and are limited to 63 bytes in length.
-
-If you do not want to use your database anymore you can remove it. For example, you can destroy it using the following function:
-```python
-import sayuDB
-
-sayuDB.drop_database('myDB')
+### Create and drop database
+To create a database, just command by
+```shell
+> python3 create database <database_name>
 ```
-
-You can import export using:
-```python
-# Exporting database (must use file name)
-sayuDB.export_database('<name_of_database>', path_='<path>/filename.ezdb')
-# Importing database
-sayuDB.import_database(path_='/<path>/filename.ezdb')
+And to drop/delete it, just command by
+```shell
+> python3 -m sayuDB drop database <database_name>
 ```
-
-#### 1.3 Creating a New Table
-You can create a new table by specifying the table name, along with all column names and their types:
-
-you must define the database first with:
-```python
-import sayuDB
-
-db = sayuDB.sayuDB(database='myDB')
+Anyway, you can also show databases by
+```shell
+> python3 -m sayuDB show databases
 ```
+### Import and export database
+To import a database, just command by
+```shell
+> python3 -m sayuDB import database <path_to_ezdb_file>
+```
+And for export it, just command by
+```shell
+> python3 -m sayuDB export database <database_name> <output_path>
+```
+### Activate or deactivate server
+If you want the database remotely online. just activate server by
+```shell
+> python3 -m sayuDB activate server <port>
+```
+_`Note: I recomended use 8787 as port`_
+then acces it within your server IP.
+if you didn't see something, try make the server public by
+```shell
+> python3 -m sayuDB setup public server <server_name> <port>
+```
+The `server_name` is your server IP or a domain that already set A record to your server IP.
+And the `port` must be same when you activate it.
 
-then
+If you want to deactivate it, just command by
+```shell
+> python3 -m sayuDB deactivate server
+```
+---
+### Using the sayuDB
+To user sayu db in python, just define it first by
 ```python
-db.create_table('people', [
-    ['name','str'],
-    ['age','int'],
-    ['city','str']
+import sayuDB # Importing the sayuDB
+db = sayuDB.sayuDB("testdb") # call the class
+```
+The parameters are
+|parameter|status|description|
+|-|-|-|
+`database`|required|the database name
+`username`|optional|the username for you sayuDB account if you access it remotely
+`password`|optional|ofc the password
+`host`|optional|the host of your database hosted in
+`as_json`|optional|this is the boolean if you want to return the database as json or just printed table.
+### Create table
+To create a table use this following function
+```python
+db.create_table("<table_name>", col=[
+    ["number","int"],
+    ["name","str"],
+    ["age","int"],
+    ["address","str"]
 ])
 ```
-sayuDB currenly support data types `str`,`int`,`float`,`dict`
+The parameters are
+|parameter|status|description|
+|-|-|-|
+`name`|required|the name of table you want to create
+`col`|required|a lists that contain list wich contain the column name and datatypes `[ ["<column_name>","<datatypes>"] ]`
 
-If you want to show the table, using this following function:
+> Note: supported datatypes `str`,`float`,`int`,`dict`
+
+### Drop table
+You can drop table by use this dollowing function
 ```python
-db.show_table(name='people')
+db.drop_table("<table_name>")
 ```
-it will returned as printed table:
+The parameters are
+|parameter|status|description|
+|-|-|-|
+`name`|required|the name of the table you want to drop/delete
+
+### Show table
+you can show the table by use this following funtion.
+```python
+db.show_table("<table_name>")
+```
+it will returned
 ```shell
-name    column    datas
-------  --------  -------
+number    name    age    address
+--------  ------  -----  ---------
 ```
+### Insert
+To insert some row to the table, you can use this following function
+```python
+db.insert_row(table="<table_name>", col="number,name,age,address", contents=[1,"Arsybai",23,"Solo, Indonesia"])
+```
+The parameters are
+|parameter|status|description|
+|-|-|-|
+`table`|required|The name of table you want to insert a row
+`col`|required|the column that you want to insert. separate by comma or you can use list
+`contents`|required|the contents/value for each column
 
-Finally, it should be mentioned that if you don't need a table any longer or want to recreate it differently you can remove it using the following function:
-```python
-db.drop_table(name='people')
-```
-
-#### 1.4 Populating a Table With Rows
-The `insert_row` function is used to populate a table with rows:
-```python
-db.insert_row(table='people', col='name,age,city', contents=['Arsybai', 23, 'Solo, Indonesia'])
-```
-_You can also use col as list. EX `['name','age','city']`_
-
-### 1.5 Querying a Table
-To retrieve data from a table, the table is queried. An `select_row` funtion is used to do this. The funtion is divided into a select list (the part that lists the columns to be returned), a table list (the part that lists the tables from which to retrieve the data), and an optional qualification (the part that specifies any restrictions). For example, to retrieve all the rows of table `people`, type:
-```python
-data = db.select_row(table='people', col='*')
-print(data)
-```
-Here `*` is a shorthand for “all columns”. So the same result would be had with:
-```python
-db.select_row(table='people', col='name,age,city')
-```
-The output should be:
+and now if you show the table it will look like this
 ```shell
-name       age  city
--------  -----  ------------------
-Arsybai     23  Solo, Indonesia
-Ataro       25  Bekasi, Indonesia
-Moepoi      21  Jakarta, Indonesia
+  number  name       age  address
+--------  -------  -----  ---------------
+       1  Arsybai     23  Solo, Indonesia
 ```
 
-If you want the output is json, use `as_json=True`:
+and you can also insert it with some function. as example I want to insert moepoi as number 2. but I want the number automatically increased.
 ```python
-data = db.select_row(table='people', col='*', as_json=True)
-print(data)
+db.insert_row(table="<table_name>", col="number,name,age,address", contents=["increase()","Moepoi",20,"Jakarta, Indonesia"])
 ```
-
-A query can be “qualified” by adding a `where` clause that specifies which rows are wanted:
-```python
-data = db.select_row(table='people', col='*', where='age=23')
-print(data)
-```
-you can also use ` contain ` for specific column that contain some value:
-```python
-data = db.select_row(table='people', col='*', where='age contain 23')
-print(data)
-```
-Result:
+So, I use '`increase()' function for it. then the table will look like this
 ```shell
-name       age  city
--------  -----  ---------------
-Arsybai     23  Solo, Indonesia
+  number  name       age  address
+--------  -------  -----  ------------------
+       1  Arsybai     23  Solo, Indonesia
+       2  Moepoi      20  Jakarta, Indonesia
 ```
+Table of functions
+|name|description|
+|-|-|
+`today()`|Generate today datetime (only work in str typedata)
+`index()`|Indexing the number of row. it can be use as id too (only work in int typedata)
+`genID()`|Generate random 5 digits ID
+`increase()`|Increase from the last row value (Only work in int typedata)
 
-You can request that the results of a query be returned in sorted order:
+### Select
+To select a table, use this following function
 ```python
-data = db.select_row(table='people', col='*', order_by='age|asc')
-print(data)
+db.select_row(table="<table_name>", col="name,age")
 ```
-_The order should be `asc` or `desc`_
+|parameter|status|description|
+|-|-|-|
+`table`:`str`|required|the name of table that you want to select
+`col`:`str`|required|the column name that you want to select. it separated by comma. or you can use `col="*"` if you want to select all comuns
+`where`:`str`|optional|select the specific row.
+`as_json`:`bool`|optional|if you already define it in the `sayuDB` class, you didn't need to do it again. the default is `False`
 
-Result:
-```shell
-name       age  city
--------  -----  ------------------
-Moepoi      21  Jakarta, Indonesia
-Arsybai     23  Solo, Indonesia
-Ataro       25  Bekasi, Indonesia
-```
-
-#### Updates
-You can update existing rows using the `update_row`.
-as example I want to set `Ataro` age to `26`:
+### Where
+Now, if you want to select specific row, you can use `where` parameter for it
+##### Equal or not equal.
+as example, I want to select `Arsybai` from table.
 ```python
-db.update_row(table='people', set_='age=26', where='name=Ataro')
+db.select_row(table="<table_name>", col="*", where="name=Arsybai")
 ```
-Look at the new state of the data:
+Otherwise, if you want to select row that isn't `Arsybai` use `where="name!=Arsybai"`
+The return will look like this
 ```shell
-name       age  city
--------  -----  ------------------
-Arsybai     23  Solo, Indonesia
-Ataro       26  Bekasi, Indonesia
-Moepoi      21  Jakarta, Indonesia
-```
+  number  name       age  address
+--------  -------  -----  ---------------
+       1  Arsybai     23  Solo, Indonesia
 
-#### Deletion
-Rows can be removed from a table using the `delete_row`:
+1 Rows
+```
+##### contains
+If you want to select row that contain someting. you can use ` contain `.
+as example I want to select name that contain `poi`
 ```python
-db.delete_row(table='people', where='name=Ataro')
+db.select_row(table="<table_name>", col="*", where="name contain poi")
+```
+The return will look like this
+```shell
+  number  name      age  address
+--------  ------  -----  ------------------
+       2  Moepoi     20  Jakarta, Indonesia
+
+1 Rows
+```
+##### Using multiple condition
+as example I want to select `Arsybai` with age `20`
+```python
+db.select_row(table="<table_name>", col="*", where="name=Arsybai && age=20")
+```
+The return will look like this
+```shell
+number    name    age    address
+--------  ------  -----  ---------
+
+0 Rows
+```
+> Note : You can use `AND ( && )`, `OR ( || )` in multiple condition. also the space between && or || is required
+
+### Update
+If you want to update a row, use this following function
+```python
+db.update_row(table="<table_name>", set_="<column_name>=<value>", where="<column_name>=<value>")
+```
+Note that update row isn't support multiple condition.
+
+Also you can update a row more efficienly with `update_row_json`
+```python
+db.update_row_json(table="<table_name>", set_={"<col1_name>":"<value1>","<col2_name>":"<value2>"}, where="<col_name>=<value>")
 ```
 
-Look at the new state of the data:
-```shell
-name       age  city
--------  -----  ------------------
-Arsybai     23  Solo, Indonesia
-Moepoi      21  Jakarta, Indonesia
+### Delete
+If you want to delete some row, use this following function
+```python
+#to delete specific data that equal with some value
+db.delete_row(table="<table_name>", where="<col_name>=<value>")
+
+#to delete specific data that contain some value
+db.delete_row(table="<table_name>", where="<col_name> contain <value>")
+
+#to delete specific data with row number
+db.delete_row(table="<table_name>", where="row <number_of_row>")
+
+#to delete specific data between row number
+db.delete_row(table="<table_name>", where="row between <number_of_rowX>-<number_of_rowY>")
 ```
+### Alter table
+To alter or updating some table, use this following function
+```python
+# to add a column
+db.alter_table_add_column(table="<table_name>", col_name="<col_name>", datatypes="<datatype>")
+
+# to remove a column
+db.alter_table_drop_column(table="<table_name>", col_name="<col_name>")
+
+# to rename a column
+db.alter_table_rename_column(table="<table_name>", col="<column_to_rename>", to_="<rename_to>")
+```
+### Clear table
+To clear table use this following function
+```python
+db.clear_table(table="<table_name>")
+```
+This will erase all rows data in the table.
 """
 )
