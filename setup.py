7fe13b329d38fef733d515e6d92369c464f76e7c
arsybai <me@arsybai.com>
2023-05-18 20:48:44 +0700

    Updated 1.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="sayuDB",
-    version="1.1.0",
+    version="1.1.4",
     author="Arsybai",
     description="Database management system based on python and JSON.",
     packages=["sayuDB"],
